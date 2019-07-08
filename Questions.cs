d92c0ca1deee60f5bf84858ff6219d9451c55929
gramakrishnamca <39645760+gramakrishnamca@users.noreply.github.com>
2019-07-08 20:15:08 +0530

    Updated with Answers as Programs for all Questions

---
+++ Questions.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace CDSPractical {
+    public class Questions {
+        /// <summary>
+        /// Given an enumerable of strings, attempt to parse each string and if 
+        /// it is an integer, add it to the returned enumerable.
+        /// 
+        /// For example:
+        /// 
+        /// ExtractNumbers(new List<string> { "123", "hello", "234" });
+        /// 
+        /// ; would return:
+        /// 
+        /// {
+        ///   123,
+        ///   234
+        /// }
+        /// </summary>
+        /// <param name="source">An enumerable containing words</param>
+        /// <returns></returns>
+        public IEnumerable<int> ExtractNumbers(IEnumerable<string> source) {
+            List<int> integerslist = new List<int>();
+            foreach (var elements in source)
+            {
+                int n;
+                bool isNumeric = int.TryParse(ele, out n);
+                if (isNumeric)
+                {
+                    integerslist.Add(n);
+                }
+            }
+            IEnumerable<int> numbers = integerslist;
+            return numbers;
+        }
+
+        /// <summary>
+        /// Given two enumerables of strings, find the longest common word.
+        /// 
+        /// For example:
+        /// 
+        /// LongestCommonWord(
+        ///     new List<string> {
+        ///         "love",
+        ///         "wandering",
+        ///         "goofy",
+        ///         "sweet",
+        ///         "mean",
+        ///         "show",
+        ///         "fade",
+        ///         "scissors",
+        ///         "shoes",
+        ///         "gainful",
+        ///         "wind",
+        ///         "warn"
+        ///     },
+        ///     new List<string> {
+        ///         "wacky",
+        ///         "fabulous",
+        ///         "arm",
+        ///         "rabbit",
+        ///         "force",
+        ///         "wandering",
+        ///         "scissors",
+        ///         "fair",
+        ///         "homely",
+        ///         "wiggly",
+        ///         "thankful",
+        ///         "ear"
+        ///     }
+        /// );
+        /// 
+        /// ; would return "wandering" as the longest common word.
+        /// </summary>
+        /// <param name="first">First list of words</param>
+        /// <param name="second">Second list of words</param>
+        /// <returns></returns>
+        public string LongestCommonWord(IEnumerable<string> first, IEnumerable<string> second) {
+            HashSet<string> lwrds = new HashSet<string>();
+            int max = 0;
+            string maxword = "";
+            foreach (var element in first)
+            {
+                lwrds.Add(element);
+
+            }
+
+            foreach (var element in second)
+            {
+                if (lwrds.Contains(element))
+                {
+                    int currentlength = element.Length;
+                    if (max < currentlength)
+                    {
+                        max = currentlength;
+                        maxword = element;
+                    }
+                }
+            }
+            return maxword;
+        }
+
+        /// <summary>
+        /// Write a method that converts kilometers to miles, given that there are
+        /// 1.6 kilometers per mile.
+        /// 
+        /// For example:
+        /// 
+        /// DistanceInMiles(16.00);
+        /// 
+        /// ; would return 10.00;
+        /// </summary>
+        /// <param name="km">distance in kilometers</param>
+        /// <returns></returns>
+        public double DistanceInMiles(double km) {
+            return km / 1.6;
+        }
+
+        /// <summary>
+        /// Write a method that converts miles to kilometers, give that there are
+        /// 1.6 kilometers per mile.
+        /// 
+        /// For example:
+        /// 
+        /// DistanceInKm(10.00);
+        /// 
+        /// ; would return 16.00;
+        /// </summary>
+        /// <param name="miles">distance in miles</param>
+        /// <returns></returns>
+        public double DistanceInKm(double miles) {
+            return miles * 1.6;
+        }
+
+        /// <summary>
+        /// Write a method that returns true if the word is a palindrome, false if
+        /// it is not.
+        /// 
+        /// For example:
+        /// 
+        /// IsPalindrome("bolton");
+        /// 
+        /// ; would return false, and:
+        /// 
+        /// IsPalindrome("Anna");
+        /// 
+        /// ; would return true.
+        /// 
+        /// Also complete the related test case for this method.
+        /// </summary>
+        /// <param name="word">The word to check</param>
+        /// <returns></returns>
+        public bool IsPalindrome(string word) {
+            string revs = "";
+            for (int i = word.Length - 1; i >= 0; i--) 
+            {
+                revs += word[i].ToString();
+            }
+            if (revs == word) // Checking whether the given word is palindrome or not  
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }            
+        }
+
+        /// <summary>
+        /// Write a method that takes an enumerable list of objects and shuffles
+        /// them into a different order.
+        /// 
+        /// For example:
+        /// 
+        /// Shuffle(new List<string>{ "one", "two" });
+        /// 
+        /// ; would return:
+        /// 
+        /// {
+        ///   "two",
+        ///   "one"
+        /// }
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public IEnumerable<object> Shuffle(IEnumerable<object> source) {
+            List<object> ilist = new List<object>();
+            foreach (object item in source)
+            {
+                ilist.Add(item);
+            }
+            ilist.Reverse();
+            IEnumerable<object> nums = integerslist;
+            return nums;
+        }
+
+        /// <summary>
+        /// Write a method that sorts an array of integers into ascending
+        /// order - do not use any built in sorting mechanisms or frameworks.
+        /// 
+        /// Complete the test for this method.
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public int[] Sort(int[] source) {
+            int tmp;
+            for (int i = 0; i < source.Length - 1; i++)
+                for (int j = i + 1; j < source.Length; j++)
+                    if (source[i] > source[j])
+                    {
+                        tmp = source[i];
+                        source[i] = source[j];
+                        source[j] = tmp;
+                    }
+            return source;
+        }    
+
+        /// <summary>
+        /// Each new term in the Fibonacci sequence is generated by adding the 
+        /// previous two terms. By starting with 1 and 2, the first 10 terms will be:
+        ///
+        /// 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
+        ///
+        /// By considering the terms in the Fibonacci sequence whose values do 
+        /// not exceed four million, find the sum of the even-valued terms.
+        /// </summary>
+        /// <returns></returns>
+        public int FibonacciSum() {
+            int maxValue = 4000000;  //By considering the terms in the Fibonacci sequence whose values do not exceed four million
+            int i = 0, j = 1, k = 0;
+            int evtSum = 0; //sum of the even-valued terms
+
+            while (c < maxValue)
+            {
+                k = i + j;
+                if (k % 2 == 0) { evtSum += k; } //Checking for the even-valued terms 
+                i = j;
+                j = k;
+            }
+
+            return evtSum;
+        }
+
+        /// <summary>
+        /// Generate a list of integers from 1 to 100.
+        /// 
+        /// This method is currently broken, fix it so that the tests pass.
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<int> GenerateList() {
+            var ret = new List<int>();
+            var numThreads = 2;
+
+            Thread[] threads = new Thread[numThreads];
+            for (var i = 0; i < numThreads; i++) {
+                threads[i] = new Thread(() => {
+                    var complete = false;
+                    while (!complete) {
+                        lock (ret)     //Added this lock condition to fix the broken code
+                        {
+                            var next = ret.Count + 1;
+                            Thread.Sleep(new Random().Next(1, 10));
+                            if (next <= 100)
+                            {
+                                ret.Add(next);
+                            }
+
+                            if (ret.Count >= 100)
+                            {
+                                complete = true;
+                            }
+                        }
+                    }                    
+                });
+                threads[i].Start();
+            }
+
+            for (var i = 0; i < numThreads; i++) {
+                threads[i].Join();
+            }
+
+            return ret;
+        }
+    }
+}
